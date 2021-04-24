d840419adeebfc83f1abb3a548dc690dba0023c4
wujiahao <wujiahao.mail@gmail.com>
2021-04-24 13:38:00 +0800

    开始看dict代码

--- src/dict.h
+++ src/dict.h
@@ -56,55 +56,43 @@
 /* Unused arguments generate annoying warnings... */
 // 如果字典的私有数据不使用时
 // 用这个宏来避免编译器错误
-#define DICT_NOTUSED(V) ((void) V)
+#define DICT_NOTUSED(V) ((void)V)
 
 /*
  * 哈希表节点
  */
-typedef struct dictEntry {
-    
+typedef struct dictEntry
+{
     // 键
     void *key;
-
     // 值
-    union {
+    union
+    {
         void *val;
         uint64_t u64;
         int64_t s64;
     } v;
-
     // 指向下个哈希表节点，形成链表
     struct dictEntry *next;
-
 } dictEntry;
-
-
 /*
  * 字典类型特定函数
  */
-typedef struct dictType {
-
+typedef struct dictType
+{
     // 计算哈希值的函数
     unsigned int (*hashFunction)(const void *key);
-
     // 复制键的函数
     void *(*keyDup)(void *privdata, const void *key);
-
     // 复制值的函数
     void *(*valDup)(void *privdata, const void *obj);
-
     // 对比键的函数
     int (*keyCompare)(void *privdata, const void *key1, const void *key2);
-
     // 销毁键的函数
     void (*keyDestructor)(void *privdata, void *key);
-    
     // 销毁值的函数
     void (*valDestructor)(void *privdata, void *obj);
-
 } dictType;
-
-
 /* This is our hash table structure. Every dictionary has two of this as we
  * implement incremental rehashing, for the old to the new table. */
 /*
@@ -112,44 +100,34 @@ typedef struct dictType {
  *
  * 每个字典都使用两个哈希表，从而实现渐进式 rehash 。
  */
-typedef struct dictht {
-    
+typedef struct dictht
+{
     // 哈希表数组
     dictEntry **table;
-
     // 哈希表大小
     unsigned long size;
-    
     // 哈希表大小掩码，用于计算索引值
     // 总是等于 size - 1
     unsigned long sizemask;
-
     // 该哈希表已有节点的数量
     unsigned long used;
-
 } dictht;
-
 /*
  * 字典
  */
-typedef struct dict {
-
+typedef struct dict
+{
     // 类型特定函数
     dictType *type;
-
     // 私有数据
     void *privdata;
-
     // 哈希表
     dictht ht[2];
-
     // rehash 索引
     // 当 rehash 不在进行时，值为 -1
     int rehashidx; /* rehashing not in progress if rehashidx == -1 */
-
     // 目前正在运行的安全迭代器的数量
     int iterators; /* number of iterators currently running */
-
 } dict;
 
 /* If safe is set to 1 this is a safe iterator, that means, you can call
@@ -165,74 +143,79 @@ typedef struct dict {
  * 如果 safe 不为 1 ，那么程序只会调用 dictNext 对字典进行迭代，
  * 而不对字典进行修改。
  */
-typedef struct dictIterator {
-        
+typedef struct dictIterator
+{
     // 被迭代的字典
     dict *d;
-
     // table ：正在被迭代的哈希表号码，值可以是 0 或 1 。
     // index ：迭代器当前所指向的哈希表索引位置。
     // safe ：标识这个迭代器是否安全
     int table, index, safe;
-
     // entry ：当前迭代到的节点的指针
     // nextEntry ：当前迭代节点的下一个节点
     //             因为在安全迭代器运作时， entry 所指向的节点可能会被修改，
     //             所以需要一个额外的指针来保存下一节点的位置，
     //             从而防止指针丢失
     dictEntry *entry, *nextEntry;
-
     long long fingerprint; /* unsafe iterator fingerprint for misuse detection */
 } dictIterator;
 
-typedef void (dictScanFunction)(void *privdata, const dictEntry *de);
+typedef void(dictScanFunction)(void *privdata, const dictEntry *de);
 
 /* This is the initial size of every hash table */
 /*
  * 哈希表的初始大小
  */
-#define DICT_HT_INITIAL_SIZE     4
+#define DICT_HT_INITIAL_SIZE 4
 
 /* ------------------------------- Macros ------------------------------------*/
 // 释放给定字典节点的值
-#define dictFreeVal(d, entry) \
+#define dictFreeVal(d, entry)     \
     if ((d)->type->valDestructor) \
-        (d)->type->valDestructor((d)->privdata, (entry)->v.val)
+    (d)->type->valDestructor((d)->privdata, (entry)->v.val)
 
 // 设置给定字典节点的值
-#define dictSetVal(d, entry, _val_) do { \
-    if ((d)->type->valDup) \
-        entry->v.val = (d)->type->valDup((d)->privdata, _val_); \
-    else \
-        entry->v.val = (_val_); \
-} while(0)
+#define dictSetVal(d, entry, _val_)                                 \
+    do                                                              \
+    {                                                               \
+        if ((d)->type->valDup)                                      \
+            entry->v.val = (d)->type->valDup((d)->privdata, _val_); \
+        else                                                        \
+            entry->v.val = (_val_);                                 \
+    } while (0)
 
 // 将一个有符号整数设为节点的值
 #define dictSetSignedIntegerVal(entry, _val_) \
-    do { entry->v.s64 = _val_; } while(0)
+    do                                        \
+    {                                         \
+        entry->v.s64 = _val_;                 \
+    } while (0)
 
 // 将一个无符号整数设为节点的值
 #define dictSetUnsignedIntegerVal(entry, _val_) \
-    do { entry->v.u64 = _val_; } while(0)
+    do                                          \
+    {                                           \
+        entry->v.u64 = _val_;                   \
+    } while (0)
 
 // 释放给定字典节点的键
-#define dictFreeKey(d, entry) \
+#define dictFreeKey(d, entry)     \
     if ((d)->type->keyDestructor) \
-        (d)->type->keyDestructor((d)->privdata, (entry)->key)
+    (d)->type->keyDestructor((d)->privdata, (entry)->key)
 
 // 设置给定字典节点的键
-#define dictSetKey(d, entry, _key_) do { \
-    if ((d)->type->keyDup) \
-        entry->key = (d)->type->keyDup((d)->privdata, _key_); \
-    else \
-        entry->key = (_key_); \
-} while(0)
+#define dictSetKey(d, entry, _key_)                               \
+    do                                                            \
+    {                                                             \
+        if ((d)->type->keyDup)                                    \
+            entry->key = (d)->type->keyDup((d)->privdata, _key_); \
+        else                                                      \
+            entry->key = (_key_);                                 \
+    } while (0)
 
 // 比对两个键
 #define dictCompareKeys(d, key1, key2) \
-    (((d)->type->keyCompare) ? \
-        (d)->type->keyCompare((d)->privdata, key1, key2) : \
-        (key1) == (key2))
+    (((d)->type->keyCompare) ? (d)->type->keyCompare((d)->privdata, key1, key2) : (key1) == (key2))
 
 // 计算给定键的哈希值
 #define dictHashKey(d, key) (d)->type->hashFunction(key)
@@ -245,9 +228,9 @@ typedef void (dictScanFunction)(void *privdata, const dictEntry *de);
 // 返回给定节点的无符号整数值
 #define dictGetUnsignedIntegerVal(he) ((he)->v.u64)
 // 返回给定字典的大小
-#define dictSlots(d) ((d)->ht[0].size+(d)->ht[1].size)
+#define dictSlots(d) ((d)->ht[0].size + (d)->ht[1].size)
 // 返回字典的已有节点数量
-#define dictSize(d) ((d)->ht[0].used+(d)->ht[1].used)
+#define dictSize(d) ((d)->ht[0].used + (d)->ht[1].used)
 // 查看字典是否正在 rehash
 #define dictIsRehashing(ht) ((ht)->rehashidx != -1)
 
@@ -261,7 +244,7 @@ dictEntry *dictReplaceRaw(dict *d, void *key);
 int dictDelete(dict *d, const void *key);
 int dictDeleteNoFree(dict *d, const void *key);
 void dictRelease(dict *d);
-dictEntry * dictFind(dict *d, const void *key);
+dictEntry *dictFind(dict *d, const void *key);
 void *dictFetchValue(dict *d, const void *key);
 int dictResize(dict *d);
 dictIterator *dictGetIterator(dict *d);
@@ -273,7 +256,7 @@ int dictGetRandomKeys(dict *d, dictEntry **des, int count);
 void dictPrintStats(dict *d);
 unsigned int dictGenHashFunction(const void *key, int len);
 unsigned int dictGenCaseHashFunction(const unsigned char *buf, int len);
-void dictEmpty(dict *d, void(callback)(void*));
+void dictEmpty(dict *d, void(callback)(void *));
 void dictEnableResize(void);
 void dictDisableResize(void);
 int dictRehash(dict *d, int n);
