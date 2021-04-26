da469580ab43ffd16e9c91c6a46e91e6a024c135
wujiahao.2021 <wujiahao.2021@bytedance.com>
2021-04-26 13:56:39 +0800

    intset源码阅读

--- src/intset.h
+++ src/intset.h
@@ -33,16 +33,12 @@
 #include <stdint.h>
 
 typedef struct intset {
-    
     // 编码方式
     uint32_t encoding;
-
     // 集合包含的元素数量
     uint32_t length;
-
     // 保存元素的数组
     int8_t contents[];
-
 } intset;
 
 intset *intsetNew(void);
